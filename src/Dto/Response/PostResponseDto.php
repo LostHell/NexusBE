4ab2adb53a04fae49ee40a5168855dc145c02a26
LostHell <losthell92@gmail.com>
2021-04-08 17:45:22 +0200

    Switch from AutoMapper to manual mapping

--- src/Dto/Response/PostResponseDto.php
+++ src/Dto/Response/PostResponseDto.php
@@ -4,11 +4,52 @@ namespace App\Dto\Response;
 
 class PostResponseDto
 {
-    public $content;
+    /**
+     * @var string
+     */
+    private $content;
 
-    public $author;
+    /**
+     * @var \DateTime
+     */
+    private $date_created;
 
-    public $date_created;
+    /**
+     * @var \DateTime
+     */
+    private $date_last_update;
 
-    public $date_last_update;
+    public function __construct(
+        string $content,
+        \DateTimeInterface $date_created,
+        \DateTimeInterface $date_last_update)
+    {
+        $this->content = $content;
+        $this->date_created = $date_created;
+        $this->date_last_update = $date_last_update;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateCreated(): \DateTime
+    {
+        return $this->date_created;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateLastUpdate(): \DateTime
+    {
+        return $this->date_last_update;
+    }
 }
