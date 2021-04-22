3c7054f3e443fa25d11bf98cc44f52a1e5718021
LostHell <losthell92@gmail.com>
2021-04-22 12:13:34 +0200

    Test

--- migrations/Version20210422101241.php
+++ migrations/Version20210422101241.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210422100621 extends AbstractMigration
+final class Version20210422101241 extends AbstractMigration
 {
     public function getDescription() : string
     {
